ff4e8b896002ad9f458268181abb540ad693f4e6
Jason Morley <hello@jbmorley.co.uk>
2021-07-02 20:12:04 +0100

    Move some more steps into the install script.

--- install.sh
+++ install.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+# Install some useful additional software.
+# N.B. This step is entirely optional but helps set up devices for development.
+sudo apt-get install --yes \
+     mosh \
+     emacs
+
 # Install and configure the GPIO expansion board.
 # https://learn.adafruit.com/circuitpython-on-any-computer-with-ft232h/linux
-sudo apt-get install --yes libusb-1.0
-curl https://raw.githubusercontent.com/jbmorley/raspberry-pi-portable/main/11-ftdi.rules | sudo tee /etc/udev/rules.d/11-ftdi.rules 
+sudo apt-get install --yes \
+     libusb-1.0
+sudo cp 11-ftdi.rules /etc/udev/rules.d/11-ftdi.rules
 
 # Install the Python requirements.
 pip3 install -r requirements.txt
